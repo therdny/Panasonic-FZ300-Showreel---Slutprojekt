6aed9ab8c19943fc373ea80c5f989684a86d566b
therdny <andreas@rdny.se>
2016-12-01 15:29:28 +0100

    Added top-of-page-link in kamera

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-  $(function() {
+$(function() {
     $("#tabs").tabs();
-  } );
+  });
 
- 
+$('body').prepend('<a href="#" class="top-of-page">Till topps!</a>');
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.top-of-page').fadeIn('slow');
+	} else {
+		$('a.top-of-page').fadeOut('slow');
+	}
+});
+
+$('a.top-of-page').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
