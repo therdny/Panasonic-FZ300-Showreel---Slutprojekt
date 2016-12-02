1c3184e97e67e9fbf7875c891f8d1cdff890c2ea
therdny <andreas@rdny.se>
2016-12-02 10:33:39 +0100

    Done with the assignment
    Added font awesome and designed the footer.

--- js/form.js
+++ js/form.js
@@ -1,18 +1,20 @@
 //function checkFunction() {
 
-//	if ($("input[name="bag"]").is(":checked")) {
-//	document.getElementById("orderedbag");
-//	orderedbag.innerHTML = "Och du har beställt en väska för 500:- till den.";
-//	}
+	//if ($("input[name="bag"]").is(":checked")) {
+	//document.getElementById("orderedbag");
+	//var orderedbag = document.getElementById("orderedbag");
+	//orderedbag.innerHTML = "Och du har beställt en väska för 500:- till den.";
+	//}
 
-//	else {
-//	document.getElementById("orderedbag");
-//	orderedbag.innerHTML = "Du valde att inte beställa en väska till den.";
-//	};
+	//else {
+	//document.getElementById("orderedbag");
+	//var orderedbag = document.getElementById("orderedbag");
+	//orderedbag.innerHTML = "Du valde att inte beställa en väska till den.";
+	//};
 
 var form = document.getElementById("order-form");
-	form.onsubmit = function(e) {
-		e.preventDefault();
+	form.onsubmit = function(event) {
+		event.preventDefault();
 		var confirmation = document.getElementById("confirmation");
 		confirmation.innerHTML = "<h4>Orderbekräftelse</h4>" + "Hej " + form.yourName.value + ". <br />" + "Du har beställt en Panasonic FZ300. <br />" + "Den kommer att skickas till " + form.yourAdress.value + " i " + form.yourCity.value + ". <br />" + "Totalpriset blir 4995:-.";
 		this.reset();
--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //This is the tabs-function
 $(function() {
     $("#tabs").tabs();
@@ -11,16 +10,24 @@ var amountScrolled = 300;
 
 $(window).scroll(function() {
 	if ( $(window).scrollTop() > amountScrolled ) {
-		$('a.top-of-page').fadeIn('slow');
+		$("a.top-of-page").fadeIn("slow");
 	} else {
 		$('a.top-of-page').fadeOut('slow');
 	}
 });
 
 //To make a cool animationeffect on the scroll-up.
-$('a.top-of-page').click(function() {
-	$('html, body').animate({
+$("a.top-of-page").click(function() {
+	$("body").animate({
 		scrollTop: 0
 	}, 500);
 	return false;
 });
+
+//Hover over the images to remove the span
+$(".span-image").hover(function(){
+$(this).fadeOut(1000);
+});
+
+//Changing the hover-effect on the p-tag (for the link in the footer)
+$("p").children().css("textDecoration", "none");
